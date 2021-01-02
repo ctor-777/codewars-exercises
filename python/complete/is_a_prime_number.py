f0a63940e0e2d3918a161b3c2d6fdd8795ed241f
Hector Rodriguez <ctor777@gmail.com>
2021-01-02 23:58:27 +0100

    Minor changes.
    added coments, added tests and changed test system on:
        -is_a_prime_number.py

--- python/complete/is_a_prime_number.py
+++ python/complete/is_a_prime_number.py
@@ -1,6 +1,7 @@
 ###this problem consist in a function that given a number returns true or false if 
 ###the number is a prime number
 
+from itertools import permutations
 import unittest
 import math
 
@@ -24,7 +25,7 @@ def is_a_prime_number_2(number):
         return True
     return False
 
-#solution three: for this solution we will be iterating from 2 to teh square root ef the number
+#solution three: for this solution we will be iterating from 2 to the square root of the number
 #O(sqrt(n)) --- we have reduced a lot the big O 
 #that's enought efficiency
 def is_a_prime_number_3(number, arg = math.sqrt):
@@ -37,9 +38,12 @@ def is_a_prime_number_3(number, arg = math.sqrt):
 
 #there's anothers solution, is not much more faster than the last one, but should be faster
 #O(?), honestly, i don't want to calculate the big O right now, good night
+#edit: the big O should be equal to the last one
+#O(sqrt(n))
 def is_a_prime_number_4(number, arg = math.sqrt):
     num_str = str(number)
     if number > 1:
+        #discard the options where the numbers end in 2, 4, 5, 6, 8, as long as a number ended in those can't be prime
         if num_str[len(num_str) - 1] == 2 or num_str[len(num_str) - 1] == 4 or num_str[len(num_str) - 1] == 5 or num_str[len(num_str) - 1] == 6 or num_str[len(num_str) - 1] == 8:
             return False
         for i in range(2, int(arg(number)) + 1):
@@ -50,68 +54,51 @@ def is_a_prime_number_4(number, arg = math.sqrt):
 
 
 ##test --- don't give them too much time, they are all quite similar
+
+#default unit test with some predefined test cases
+def default_test(testcase, function):
+    a = 4
+    b = 13
+    c = 23
+    d = 20
+    e = 10007
+    testcase.assertEqual(function(a), False)
+    testcase.assertEqual(function(b), True)
+    testcase.assertEqual(function(c), True)
+    testcase.assertEqual(function(d), False)
+    testcase.assertEqual(function(e), True)
+
+#test to ensure that all solutions give the same answer over a range(depth) of parameters
+def equality_test(testcase, depth, *args):
+    permuts = permutations(args, 2)
+    for i in permuts:
+        function1 = i[0]
+        function2 = i[1]
+        for j in range(depth):
+            testcase.assertEqual(function1(j), function2(j))
+
+
 class test(unittest.TestCase):
+    #testing function 1
     def test_1(self):
-        a = 4
-        b = 13
-        c = 23
-        d = 20
-        e = 10007
-        self.assertEqual(is_a_prime_number_1(a), False)
-        self.assertEqual(is_a_prime_number_1(b), True)
-        self.assertEqual(is_a_prime_number_1(c), True)
-        self.assertEqual(is_a_prime_number_1(d), False)
-        self.assertEqual(is_a_prime_number_1(e), True)
+        default_test(self, is_a_prime_number_1)
 
+    #testing function 2
     def test_2(self):
-        a = 4
-        b = 13
-        c = 23
-        d = 20
-        e = 9973
-        self.assertEqual(is_a_prime_number_2(a), False)
-        self.assertEqual(is_a_prime_number_2(b), True)
-        self.assertEqual(is_a_prime_number_2(c), True)
-        self.assertEqual(is_a_prime_number_2(d), False)
-        self.assertEqual(is_a_prime_number_2(e), True)
+        default_test(self, is_a_prime_number_2)
 
+    #testing function 3
     def test_3(self):
-        a = 4
-        b = 13
-        c = 23
-        d = 20
-        e = 9973
-        self.assertEqual(is_a_prime_number_3(a), False)
-        self.assertEqual(is_a_prime_number_3(b), True)
-        self.assertEqual(is_a_prime_number_3(c), True)
-        self.assertEqual(is_a_prime_number_3(d), False)
-        self.assertEqual(is_a_prime_number_3(e), True)
+        default_test(self, is_a_prime_number_3)
 
-    def test_3(self):
-        pass
-        a = 4
-        b = 13
-        c = 23
-        d = 20
-        e = 9973
-        self.assertEqual(is_a_prime_number_3(a), False)
-        self.assertEqual(is_a_prime_number_3(b), True)
-        self.assertEqual(is_a_prime_number_3(c), True)
-        self.assertEqual(is_a_prime_number_3(d), False)
-        self.assertEqual(is_a_prime_number_3(e), True)
-
+    #testing function 4
     def test_4(self):
-        pass
-        a = 4
-        b = 13
-        c = 23
-        d = 20
-        e = 9973
-        self.assertEqual(is_a_prime_number_4(a), False)
-        self.assertEqual(is_a_prime_number_4(b), True)
-        self.assertEqual(is_a_prime_number_4(c), True)
-        self.assertEqual(is_a_prime_number_4(d), False)
-        self.assertEqual(is_a_prime_number_4(e), True)
+        default_test(self, is_a_prime_number_4)
+
+    #testing that all functios give the same answers
+    def test_5(self):
+        depth = 10000
+        equality_test(self, depth, is_a_prime_number_1, is_a_prime_number_2, is_a_prime_number_3, is_a_prime_number_4)
 
 
 if __name__ == "__main__":
